92dc307faf0fe38d8a514ccf7d2490adfa4b8f74
amaz11 <k.m.shaonred@gmail.com>
2022-12-08 18:10:37 +0600

    last commit for today

--- src/app/budgetcontent/budgetcontent.component.ts
+++ src/app/budgetcontent/budgetcontent.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as apex from 'ng-apexcharts';
 
 @Component({
   selector: 'app-budgetcontent',
   templateUrl: './budgetcontent.component.html',
-  styleUrls: ['./budgetcontent.component.scss']
+  styleUrls: ['./budgetcontent.component.scss'],
 })
 export class BudgetcontentComponent implements OnInit {
+  series!: apex.ApexAxisChartSeries;
+  chart!: apex.ApexChart;
+  title!: apex.ApexTitleSubtitle;
+  xaxis!: apex.ApexXAxis;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.initializeChartOptions();
   }
-
+  private initializeChartOptions(): void {
+    this.series = [
+      {
+        name: 'Series 1',
+        data: [80, 50, 30, 40, 100, 20],
+      },
+    ];
+    this.chart = {
+      height: 250,
+      width: 450,
+      type: 'radar',
+    };
+    this.title = {
+      text: '',
+    };
+    this.xaxis = {
+      categories: ['January', 'February', 'March', 'April', 'May', 'June'],
+    };
+  }
 }
